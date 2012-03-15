4b9f4d93377313b93e7f631d6bb5f22e762b1bcc
Gabriel <gabrielhilal@gmail.com>
2012-03-15 01:48:11 +0000

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,8 +35,7 @@ Product.create(title: 'Programming Ruby 1.9',
 Product.create(title: 'Rails Test Prescriptions',
   description: %{Rails Test Prescriptions is a comprehensive guide to testing
   Rails applications, covering Test-Driven Development from both a
-  theoretical perspective (why to test) and from a practical perspective
-  (how to test effectively).},
+  theoretical perspective...},
   image: 'rtp.jpg',
   price: 34.95,
   category_id: 1,
@@ -53,8 +52,7 @@ Product.create(title: 'The C Programming Language',
 # . . .
 Product.create(title: 'Expert C Programming',
   description: %{Defying the stereotypical notion that technical books tend to be boring,
-  Expert C Programming offers a lively and often humorous look at many aspects of C--from
-  how memory is laid out to the details of pointers and arrays.},
+  Expert C Programming offers a lively and often...},
   image: 'expertc.jpg',
   price: 41.35,
   category_id: 2,
@@ -71,8 +69,7 @@ Product.create(title: 'Ruminations on C++',
 # . . .
 Product.create(title: 'Sams Teach Yourself Java in 24 Hours',
   description: %{In just 24 lessons of one hour or less, you can learn how to create Java applications
-  with the free NetBeans visual editing tools. Using a straightforward, step-by-step approach, popular
-  author Rogers Cadenhead helps you master the skills...},
+  with the free NetBeans visual editing tools. Using a straightforward...},
   image: 'java1.jpg',
   price: 27.40,
   category_id: 3,
@@ -80,8 +77,7 @@ Product.create(title: 'Sams Teach Yourself Java in 24 Hours',
 # . . .
 Product.create(title: 'Java Pocket Guide',
   description: %{How many times have you reached an impasse while writing code because you couldn't remember
-  how something in Java worked? This new pocket guide is designed to keep you moving. Concise, convenient and
-  easy to use, the Java Pocket Guide gives you Java stripped down to its bare essentials...},
+  how something in Java worked? This new pocket guide is designed to keep you....},
   image: 'java2.jpg',
   price: 18.99,
   category_id: 3,
@@ -89,8 +85,7 @@ Product.create(title: 'Java Pocket Guide',
 # . . .
 Product.create(title: 'Essential PHP Security',
   description: %{You've heard the nasty stories about PHP sites being wiped off the web by evil hackers?
-  Sadly it's not scare-mongering as it does happen, and as much as we love PHP it needs proper security
-  to keep your site safe from harm.},
+  Sadly it's not scare-mongerin...},
   image: 'php1.jpg',
   price: 29.70,
   category_id: 4,
@@ -107,8 +102,7 @@ Product.create(title: 'PHP: The Complete Reference',
 # . . .
 Product.create(title: 'Learning ASP.NET 3.5',
   description: %{With this book, you will learn how to create engaging and interactive web applications
-  using the latest version of the world's most popular web development platform: ASP.NET with AJAX, built
-  on the productivity-enhancing features of Visual Studio 2008.},
+  using the latest version of the world's...},
   image: 'asp.jpg',
   price: 32.50,
   category_id: 5,
@@ -124,8 +118,7 @@ Product.create(title: 'Sams Teach Yourself HTML and CSS in 24 Hours',
 # . . .
 Product.create(title: 'HTML & CSS: Design and Build Web Sites',
   description: %{Every day, more and more people want to learn some HTML and CSS. Joining the
-  professional web designers and programmers are new audiences who need to know a little bit of
-  code at work ...},
+  professional web designers and...},
   image: 'css2.jpg',
   price: 23.99,
   category_id: 6,
